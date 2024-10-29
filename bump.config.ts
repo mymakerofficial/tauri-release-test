a58708a616423b5661e64c1d92121ad4da8f997a
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-10-29 15:57:57 +0100

    fix bump

--- bump.config.ts
+++ bump.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'bumpp'
 
 export default defineConfig({
-  files: ['package.json', 'package-lock.json', './src-tauri/Cargo.toml', './src-tauri/tauri.conf.json'],
+  files: ['package.json', 'package-lock.json', './src-tauri/Cargo.toml', './src-tauri/Cargo.lock', './src-tauri/tauri.conf.json'],
   // update the lockfile because bumpp can't properly update it
-  execute: 'cargo generate-lockfile --offline --manifest-path ./src-tauri/Cargo.toml',
+  execute: 'cargo generate-lockfile --offline --quiet --manifest-path ./src-tauri/Cargo.toml',
 })
